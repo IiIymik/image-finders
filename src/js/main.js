b178648f2be6bb4a566873930524c1c5fe8801c6
IiIymik <71165032+IiIymik@users.noreply.github.com>
2021-05-22 07:47:18 +0300

    add lightBox

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,14 @@
 import list from '../templates/partials/list-card.hbs';
 import ImageApiService from './apiService.js';
-// import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basiclightbox.min.js';
+import * as basicLightbox from 'basiclightbox';
 import LoadMoreBTN from './load-more-btn';
 
 const refs = {
     formEl: document.getElementById('search-form'),
     boxEl: document.querySelector('.js-card__list'),
 }
+
 
 const imageApiService = new ImageApiService();
 const loadMoreBTN = new LoadMoreBTN({
@@ -16,7 +18,7 @@ const loadMoreBTN = new LoadMoreBTN({
 
 
 refs.formEl.addEventListener('submit', onSearch);
-loadMoreBTN.refs.button.addEventListener('click', disableAndFetch)
+loadMoreBTN.refs.button.addEventListener('click', disableAndFetch);
 
 function onSearch(e) {
     e.preventDefault();
@@ -43,6 +45,10 @@ function disableAndFetch() {
 function appendImageMarcup(arrImages) {
     loadMoreBTN.enable();
     refs.boxEl.insertAdjacentHTML('beforeend', list(arrImages));
+    const imgLink = document.querySelectorAll('.gallery_item');
+    imgLink.forEach(e => {
+        e.addEventListener('click', openBigImg);
+    });
 
     window.scrollTo({
     top: document.body.scrollHeight,
@@ -50,14 +56,15 @@ function appendImageMarcup(arrImages) {
 });
 }
 
-
-
-
 function clearContainer() {
     refs.boxEl.innerHTML = "";
 }
 
-// const largeImg = basicLightbox.create(`
-// <img src="" alt="">`)
+function openBigImg(e) {
+    const currCard = e.currentTarget.children[0].childNodes[1].childNodes[1].children[0].currentSrc;
+    const instance = basicLightbox.create(`
+    <img src="${currCard}" width="800" height="600">
+`);
 
-// // console.log(largeImg.show());
+    instance.show();
+}
